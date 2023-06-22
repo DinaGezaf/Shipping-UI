7f627be7cba2e5a400059b3ddacb1cc6c85fb85c
amrhweedy <amrhweedy@gmail.com>
2023-06-22 05:17:20 +0300

    new repo

---
+++ ShippingUI/src/app/app-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DisplayEmployeeComponent } from './components/Employee/display/display.component';
+import { AddEmployeeComponent } from './components/Employee/add/add.component';
+import { EditEmployeeComponent } from './components/Employee/edit/edit.component';
+import { DisplayTraderComponent } from './components/Trader/display/display.component';
+import { AddTraderComponent } from './components/Trader/add/add.component';
+import { DisplayPrivellageComponent } from './components/Privellge/display/display.component';
+import { AddPrivellageComponent } from './components/Privellge/add/add.component';
+import { EditPrevillageComponent } from './components/Privellge/edit/edit.component';
+import { DisplayGovernmentComponent } from './components/Government/display/display.component';
+import { DisplayBranchComponent } from './components/Branch/display-branch/display-branch.component';
+import { AddBranchComponent } from './components/Branch/add/add.component';
+import { EditBranchComponent } from './components/Branch/edit/edit.component';
+import DisplaySalesComponent from './components/Sales Representator/display-sales/display-sales.component';
+import { AddSalesComponent } from './components/Sales Representator/add/add.component';
+import { EditSalesComponent } from './components/Sales Representator/edit-sales/edit-sales.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/employee', pathMatch: 'full' },
+  { path: 'employee', component: DisplayEmployeeComponent },
+  { path: 'employee/add', component: AddEmployeeComponent },
+  { path: 'employee/edit/:id', component: EditEmployeeComponent },
+  { path: 'trader', component: DisplayTraderComponent },
+  { path: 'trader/add', component: AddTraderComponent },
+  { path: 'trader/edit/:id', component: EditEmployeeComponent },
+  { path: 'privilege', component: DisplayPrivellageComponent },
+  { path: 'privilege/add', component: AddPrivellageComponent },
+  { path: 'privilege/edit/:id', component: EditPrevillageComponent },
+  { path: 'government', component: DisplayGovernmentComponent },
+  { path: 'branch', component: DisplayBranchComponent },
+  { path: 'branch/add', component: AddBranchComponent },
+  { path: 'branch/edit/:id', component: EditBranchComponent },
+  { path: 'salesRepresentator', component: DisplaySalesComponent },
+  { path: 'salesRepresentator/add', component: AddSalesComponent },
+  { path: 'salesRepresentator/edit/:id', component: EditSalesComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
